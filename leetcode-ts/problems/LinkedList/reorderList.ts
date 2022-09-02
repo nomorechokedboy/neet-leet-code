7dfdd544d24988489d476141b747c8458e248a87
nomorechokedboy <haiduongit@protonmail.com>
2022-09-02 20:08:58 +0700

    feat: solution for reorder list with typescript

--- leetcode-ts/problems/LinkedList/reorderList.ts
+++ leetcode-ts/problems/LinkedList/reorderList.ts
@@ -1,16 +1,84 @@
-import { ListNode } from '.';
+import { ListNode, mergeTwoList, reverseList } from '.';
 
 export const reorderList = (head: ListNode | null): void => {
-  let n1 = head;
-  while (n1?.next?.next) {
-    n1 = n1?.next;
+  let trail = head;
+
+  while (trail?.next && trail?.next?.next) {
+    let iter = head?.next;
+    while (iter?.next?.next) {
+      iter = iter.next;
+    }
+
+    if (!iter) throw Error('Iter is null');
+    if (!iter.next) throw Error('Iter next is null');
+
+    const temp = trail.next;
+    iter.next.next = temp;
+    trail.next = iter.next;
+    iter.next = null;
+    if (trail.next && trail.next.next) trail = trail.next.next;
+  }
+};
+
+export const reorderlistOptimal = (head: ListNode | null): void => {
+  let slow = head,
+    fast = head?.next;
+  while (slow && fast && fast.next) {
+    slow = slow?.next;
+    fast = fast.next.next;
   }
 
-  if (!n1?.next) return;
+  // if (!slow?.next) throw Error('Slow next is null');
+  // let l2 = reverseList(slow.next);
+  // slow.next = null;
 
-  if (!head?.next) return;
+  if (!slow) throw Error('Dmm');
+  let l2: typeof head = null;
+  let iter = slow?.next;
+  slow.next = null;
+  while (iter) {
+    const temp = iter.next;
+    iter.next = l2;
 
-  n1.next.next = head.next;
-  head.next = n1.next;
-  n1.next = null;
+    l2 = iter;
+    iter = temp;
+  }
+
+  while (l2) {
+    if (!head) throw Error('Head is null');
+    const temp1 = head?.next,
+      temp2 = l2.next;
+
+    head.next = l2;
+    l2.next = temp1;
+
+    head = temp1;
+    l2 = temp2;
+  }
+};
+
+export const reorderListArray = (head: ListNode | null): void => {
+  const list: (ListNode | null)[] = [];
+  let iter = head;
+  while (iter) {
+    list.push(iter);
+    iter = iter.next;
+  }
+
+  let i = 0,
+    j = list.length - 1;
+  while (true) {
+    let temp1 = list[i],
+      temp2 = list[j];
+    if (!temp1) throw Error('Fuck this shit im out');
+    if (!temp2) throw Error('Fuck this shit im out');
+    if (j - i === 1 || j - i === 0) {
+      temp2.next = null;
+      break;
+    }
+
+    temp1.next = temp2;
+    temp2.next = list[i + 1];
+    i++, j--;
+  }
 };
--- leetcode-ts/test/LinkedList/reorderList.spec.ts
+++ leetcode-ts/test/LinkedList/reorderList.spec.ts
@@ -1,20 +1,31 @@
-import { reorderList } from '../../problems/LinkedList';
+import {
+  reorderListArray,
+  reorderlistOptimal,
+} from '../../problems/LinkedList';
 import { generateListNode } from '../helper';
 
 describe('Reorder list problem (medium) on leetcode: https://leetcode.com/problems/reorder-list/', () => {
   it('Should return 1->4->3->2 for input 1->2->3->4', () => {
     const expected = generateListNode([1, 4, 2, 3]);
     const head = generateListNode([1, 2, 3, 4]);
+    const headCp = generateListNode([1, 2, 3, 4]);
 
-    reorderList(head);
+    reorderlistOptimal(headCp);
+    reorderListArray(head);
+
     expect(head).toEqual(expected);
+    expect(headCp).toEqual(expected);
   });
 
-  it('Should return 1->5->2->3->4 for input 1->2->3->4->5', () => {
-    const expected = generateListNode([1, 5, 2, 3, 4]);
+  it('Should return 1->5->2->4->3 for input 1->2->3->4->5', () => {
+    const expected = generateListNode([1, 5, 2, 4, 3]);
     const head = generateListNode([1, 2, 3, 4, 5]);
+    const headCp = generateListNode([1, 2, 3, 4, 5]);
 
-    reorderList(head);
+    reorderlistOptimal(headCp);
+    reorderListArray(head);
+
     expect(head).toEqual(expected);
+    expect(headCp).toEqual(expected);
   });
 });
